59e34338dbe59819f34dca90de16d89c3506193c
ianculovici <38501451+ianculovici@users.noreply.github.com>
2018-04-19 12:38:05 -0500

    Initial code submission

---
+++ README.md
@@ -0,0 +1,28 @@
+# SystemD Script Example
+
+This example shows a shell script that can be called in a SystemD service and how to setup the service itself. For CentOS, SystemD was introduced in CentOS 7.0 and represents a change from the way services were managed before (init.d).
+The main script (file) that is being called by the services and addresses all the commands is called here ```systemd_example.sh```.
+
+The script calls another script called ```sample_service.sh```, which can be any command that starts the program that runs as a service. This particular script or program does not have to accept commands like start and stop, since the main service script (systemd_example.sh) implements those.
+
+This can be tested by running commands like:
+```
+systemd_example.sh start```
+```
+systemd_example.sh status```
+```
+systemd_example.sh stop```
+```
+systemd_example.sh restart```
+Once confirming that these commands work, the service can be implemented. The service file needs to be created as ```/etc/systemd/system/sample_service.service```.
+
+Once this file is created, the only thing left is to enable the service (make it automatically start on reboot) and start/stop as a service using the SystemD command.
+```
+systemctl enable sample_service```
+```
+systemctl start sample_service```
+```
+systemctl stop sample_service```
+
+
+See more details at https://dataandtechnology.wordpress.com/2018/03/01/systemd-service-and-script-example/.
---
+++ systemd_example.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+
+# This service only runs as root
+[ "$(id -u)" = 0 ] && {
+    PreCmd="/bin/su - ${USER} -c"
+} || {
+      echo $0 must run as root.
+    exit 1
+}
+
+# CMD is the actual command that runs the service (e.g. httpd)
+
+SERVICE_DIR=/opt/sample_script
+CMD="$SERVICE_DIR/sample_service"
+SERVICE_NAME=SampleService
+PID_FILE_DIR=/var/run/sample_service
+PID_FILE=${PID_FILE_DIR}/sample.pid
+LOGFILE=/var/logs/sample_service.log
+LOGFILEDATE=`date '+%F:%T'`
+USER=sample_user
+USAGE_TEXT="Usage: $0 start|stop|restart|status"
+
+start_service () {
+      if [ ! -f $PID_FILE ]; then
+            echo -n "Starting $SERVICE_NAME... "
+            PID=`${PreCmd} "nohup $CMD >> ${LOGFILE} 2>&1 </dev/null & echo \\\$! "`
+            echo $PID > $PID_FILE
+            chown $USER:$USER $PID_FILE
+            echo "Started!"
+            ${PreCmd} "echo $0: $SERVICE_NAME startup $LOGFILEDATE" >> "${LOGFILE}"
+      else
+            echo "$SERVICE_NAME is already running. PID file exists."
+      fi
+}
+
+stop_service () {
+    status_service do_stop
+}
+
+status_service () {
+    SERVICE_STATUS=""
+      if [ -f $PID_FILE ]; then
+            PID=$(cat $PID_FILE);
+            if [ "$PID" = "" ]; then
+                  echo "$SERVICE_NAME is in an unknown state. Please restart. "
+            else
+                  is_running=`ps -e | grep $PID | wc -l`
+                  if [ "$is_running" = "1" ]; then
+                        if [ "$1" = "" ]; then
+                        # this is just status
+                              echo "$SERVICE_NAME is running"
+                        else
+                              if [ "$1" = "do_stop" ]; then
+                                    echo -n "$SERVICE_NAME stopping... "
+                                    kill "$PID"
+                                    echo "Stopped!"
+                                    rm $PID_FILE
+                                    ${PreCmd} "echo $0: [$LOGFILEDATE] $SERVICE_NAME shutdown" >> "${LOGFILE}"
+                              fi
+                        fi
+                  else
+                        rm $PID_FILE
+                        echo "$SERVICE_NAME is not running. PID file cleaned up."
+                  fi
+            fi
+      else
+            echo "$SERVICE_NAME is not running."
+      fi
+}
+
+[ "$1" = "" ] && {
+    echo $USAGE_TEXT
+    echo ""
+    exit 1
+}
+
+[ -d ${PID_FILE_DIR} ] || {
+        mkdir -p $PID_FILE_DIR
+        chown $USER:$USER $PID_FILE_DIR
+}
+
+case $1 in
+    start)
+        cd ${SERVICE_DIR}
+        start_service && sleep 1
+    ;;
+    stop)
+        stop_service
+    ;;
+    restart)
+        stop_service
+        sleep 2
+        start_service && sleep 1
+    ;;
+    status)
+        status_service
+esac
